6ce4d9ff9d2cae0d910b95d8c2ca2082138850d6
Gabrielneves05 <gneves17@outlook.com>
2024-10-27 16:17:43 -0300

    wip

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon');
-const menuLinks = document.querySelector('.menu-links');
+const hamburgerIcon = document.querySelector('.hamburger-icon')
+const menuLinks = document.querySelector('.menu-links')
 
-const menuItems = document.querySelectorAll('.menu-links a');
+const menuItems = document.querySelectorAll('.menu-links a')
 
-const btnCv = document.querySelector('.btn-cv');
-const btnContact = document.querySelector('.btn-contact');
+const btnCv = document.querySelector('.btn-cv')
+const btnContact = document.querySelector('.btn-contact')
 
-const body = document.querySelector('body');
+const body = document.querySelector('body')
 
 hamburgerIcon.addEventListener('click', () => {
-    hamburgerIcon.classList.toggle('open');
-    menuLinks.classList.toggle('open');
+    hamburgerIcon.classList.toggle('open')
+    menuLinks.classList.toggle('open')
 
     if(hamburgerIcon.classList.contains('open')) {
-        hamburgerIcon.classList.remove('close');
-        menuLinks.classList.remove('close');
-        body.classList.add('no-scroll');
+        hamburgerIcon.classList.remove('close')
+        menuLinks.classList.remove('close')
+        body.classList.add('no-scroll')
     } else {
-        hamburgerIcon.classList.add('close');
-        menuLinks.classList.add('close');
-        body.classList.remove('no-scroll');
+        hamburgerIcon.classList.add('close')
+        menuLinks.classList.add('close')
+        body.classList.remove('no-scroll')
     }
-});
+})
 
 menuItems.forEach(item => {
     item.addEventListener('click', () => {
-        hamburgerIcon.classList.remove('open');
-        menuLinks.classList.remove('open');
-        hamburgerIcon.classList.add('close');
-        menuLinks.classList.add('close');
-        body.classList.remove('no-scroll');
+        hamburgerIcon.classList.remove('open')
+        menuLinks.classList.remove('open')
+        hamburgerIcon.classList.add('close')
+        menuLinks.classList.add('close')
+        body.classList.remove('no-scroll')
     })
-});
+})
 
 btnCv.addEventListener('click', () => {
-    window.open('./assets/curriculo-gabrielneves.att.pdf');
-});
+    window.open('./assets/curriculo-dev.pdf')
+})
 
 btnContact.addEventListener('click', (event) => {
-    event.preventDefault();
-    window.location.href = './#contact';
-});
+    event.preventDefault()
+    window.location.href = './#contact'
+})
